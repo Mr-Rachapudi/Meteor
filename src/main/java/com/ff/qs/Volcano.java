b3e8b861e8430f865cc9a45a1e16cebfa32333d2
karthik <karthik@bitchemy.net>
2016-01-14 16:09:43 +0530

    my initial commit

---
+++ src/main/java/com/ff/qs/Volcano.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ff.qs;
+
+/**
+ *
+ * @author karthik
+ */
+public class Volcano {
+    
+    public static void main(String[] args) {
+        System.out.println("Test Volcano");
+    }
+    
+}
